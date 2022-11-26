74d5b0c6e50aa9d8a421834ce51b0705d9b349bd
bjalal21 <mohammadbima.20089@mhs.unesa.ac.id>
2022-11-26 14:55:42 +0700

    update login 2 (26/11/2022)

--- logout.php
+++ logout.php
@@ -1,17 +1,8 @@
 <?php 
+
 session_start();
-$_SESSION['session_username'] = "";
-$_SESSION['session_password'] = "";
 session_destroy();
 
-$cookie_name = "cookie_username";
-$cookie_value = "";
-$time = time() - (60 * 60);
-setcookie($cookie_name,$cookie_value,$time,"/");
+header("Location: login.php");
 
-$cookie_name = "cookie_password";
-$cookie_value = "";
-$time = time() - (60 * 60);
-setcookie($cookie_name,$cookie_value,$time,"/");
-
-header("location:login.php");
+?>
