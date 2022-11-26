790d490e17d6ae6e5df40dd79569716252f7afc5
bimajalal21 <bimajalal21@gmail.com>
2022-11-26 21:23:58 +0700

    Update_Login_3 (26/11/2022)

--- database.sql
+++ database.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2021 at 07:21 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.26
+-- Generation Time: Nov 27, 2020 at 03:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,36 +19,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `login`
+-- Database: `Data_saya
+
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `login` (`username`, `password`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(8, 'Murahsenyum', 'murahsenyum@gmail.com', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login`
+-- Indexes for table `users`
 --
-ALTER TABLE `login`
-  ADD UNIQUE KEY `username` (`username`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- login.php
+++ login.php
@@ -1,120 +1,60 @@
-<?php 
+<?php
+
+include 'config.php';
+
 session_start();
 
-//atur koneksi ke database
-$host_db    = "localhost";
-$user_db    = "root";
-$pass_db    = "";
-$nama_db    = "login";
-$koneksi    = mysqli_connect($host_db,$user_db,$pass_db,$nama_db);
-//atur variabel
-$err        = "";
-$username   = "";
-$ingataku   = "";
+error_reporting(0);
 
-if(isset($_COOKIE['cookie_username'])){
-    $cookie_username = $_COOKIE['cookie_username'];
-    $cookie_password = $_COOKIE['cookie_password'];
-
-    $sql1 = "select * from login where username = '$cookie_username'";
-    $q1   = mysqli_query($koneksi,$sql1);
-    $r1   = mysqli_fetch_array($q1);
-    if($r1['password'] == $cookie_password){
-        $_SESSION['session_username'] = $cookie_username;
-        $_SESSION['session_password'] = $cookie_password;
-    }
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
 }
 
-if(isset($_SESSION['session_username'])){
-    header("location:anggota.php");
-    exit();
+if (isset($_POST['submit'])) {
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+	$result = mysqli_query($conn, $sql);
+	if ($result->num_rows > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['username'] = $row['username'];
+		header("Location: index-aftr.html");
+	} else {
+		echo "<script>alert('Username Or Password Is Wrong')</script>";
+	}
 }
 
-if(isset($_POST['login'])){
-    $username   = $_POST['username'];
-    $password   = $_POST['password'];
-    $ingataku   = $_POST['ingataku'];
-
-    if($username == '' or $password == ''){
-        $err .= "<li>Silakan masukkan username dan juga password.</li>";
-    }else{
-        $sql1 = "select * from login where username = '$username'";
-        $q1   = mysqli_query($koneksi,$sql1);
-        $r1   = mysqli_fetch_array($q1);
-
-        if($r1['username'] == ''){
-            $err .= "<li>Username <b>$username</b> tidak tersedia.</li>";
-        }elseif($r1['password'] != md5($password)){
-            $err .= "<li>Password yang dimasukkan tidak sesuai.</li>";
-        }       
-        
-        if(empty($err)){
-            $_SESSION['session_username'] = $username; //server
-            $_SESSION['session_password'] = md5($password);
-
-            if($ingataku == 1){
-                $cookie_name = "cookie_username";
-                $cookie_value = $username;
-                $cookie_time = time() + (60 * 60 * 24 * 30);
-                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
-
-                $cookie_name = "cookie_password";
-                $cookie_value = md5($password);
-                $cookie_time = time() + (60 * 60 * 24 * 30);
-                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
-            }
-            header("location:index.html");
-        }
-    }
-}
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="stylephp.css">
+
+	<title>FSC | LOGIN</title>
 </head>
 <body>
-<div class="container my-4">    
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-        <div class="panel panel-info" >
-            <div class="panel-heading">
-                <div class="panel-title">Login dan Masuk Ke Sistem</div>
-            </div>      
-            <div style="padding-top:30px" class="panel-body" >
-                <?php if($err){ ?>
-                    <div id="login-alert" class="alert alert-danger col-sm-12">
-                        <ul><?php echo $err ?></ul>
-                    </div>
-                <?php } ?>                
-                <form id="loginform" class="form-horizontal" action="" method="post" role="form">       
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="text" class="form-control" name="username" value="<?php echo $username ?>" placeholder="username">                                        
-                    </div>
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                    </div>
-                    <div class="input-group">
-                        <div class="checkbox">
-                        <label>
-                            <input id="login-remember" type="checkbox" name="ingataku" value="1" <?php if($ingataku == '1') echo "checked"?>> Ingat Aku
-                        </label>
-                        </div>
-                    </div>
-                    <div style="margin-top:10px" class="form-group">
-                        <div class="col-sm-12 controls">
-                            <input type="submit" name="login" class="btn btn-success" value="Login"/>
-                        </div>
-                    </div>
-                </form>    
-            </div>                     
-        </div>  
-    </div>
-</div>
+	<div class="container">
+		<form action="" method="POST" class="login-email">
+			<p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+			<div class="input-group">
+				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+			</div>
+			<div class="input-group">
+				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+			</div>
+			<div class="input-group">
+				<button name="submit" class="btn">Login</button>
+			</div>
+			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+			<p class="login-register-text"><a href="index.html">Back To Home</a>.</p>
+		</form>
+	</div>
 </body>
 </html>
